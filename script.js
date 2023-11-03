a1d8f677c5faca310425e4382ac386f9cd7d44fa
kevinngonzales <kevinngstem@gmail.com>
2023-11-03 12:31:21 -0700

    added files

---
+++ script.js
@@ -0,0 +1,11 @@
+// Create an HTML and JS file and link them with the script tag
+// Use a prompt()to ask the user for their name 
+// Concatenate the user’s name into a welcome message
+// Use a DOM method to output the welcome message into an html element
+
+
+const userName = prompt("What is your name?");
+
+const greeting = document.getElementById("greeting");
+
+greeting.innerText ="Welcome, " + userName;
